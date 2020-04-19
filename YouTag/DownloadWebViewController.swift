376ea50d844962913b4d37bb262dc70566b02b8d
youstanzr <13307813+youstanzr@users.noreply.github.com>
2020-04-19 13:42:12 +0300

    Added url field and progress bar

--- YouTag/DownloadWebViewController.swift
+++ YouTag/DownloadWebViewController.swift
@@ -13,13 +13,32 @@ protocol DownloadWebViewDelegate: class {
 	func retrievedVideoLink(videoLink: String)
 }
 
-class DownloadWebViewController: UIViewController {
+class DownloadWebViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
 
 	weak var delegate: DownloadWebViewDelegate!
 	let webView = WKWebView()
+	let urlField: UITextField = {
+		let txtField = UITextField()
+		txtField.text = "https://www.youtube.com"
+		txtField.backgroundColor = .clear
+		txtField.textAlignment = .left
+		txtField.font = UIFont.init(name: "DINCondensed-Bold", size: 17)
+		txtField.autocorrectionType = .no
+		txtField.placeholder = "URL"
+		txtField.addPadding(padding: .equalSpacing(10))
+		txtField.returnKeyType = .go
+		txtField.layer.borderWidth = 3.0
+		txtField.layer.borderColor = UIColor.lightGray.cgColor
+		txtField.keyboardType = .URL
+		return txtField
+	}()
+	let progressBar: UIProgressView = {
+		let pBar = UIProgressView()
+		pBar.tintColor = GraphicColors.orange
+		return pBar
+	}()
 	let webToolbar = UIToolbar()
-	let startUrl = URL(string: "https://www.youtube.com")
-
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -42,14 +61,37 @@ class DownloadWebViewController: UIViewController {
 		webToolbar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
 		webToolbar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
 
+		urlField.delegate = self
+		self.view.addSubview(urlField)
+		urlField.translatesAutoresizingMaskIntoConstraints = false
+		urlField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 34).isActive = true
+		urlField.heightAnchor.constraint(equalToConstant: 34).isActive = true
+		urlField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+		urlField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+		webView.navigationDelegate = self
 		webView.allowsBackForwardNavigationGestures = true
-		webView.load(URLRequest(url: startUrl!))
+		webView.load(URLRequest(url: URL(string: urlField.text!)!))
 		self.view.addSubview(webView)
 		webView.translatesAutoresizingMaskIntoConstraints = false
-		webView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 34).isActive = true
+		webView.topAnchor.constraint(equalTo: self.urlField.bottomAnchor).isActive = true
 		webView.bottomAnchor.constraint(equalTo: webToolbar.topAnchor).isActive = true
 		webView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
 		webView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+		
+		self.view.addSubview(progressBar)
+		progressBar.translatesAutoresizingMaskIntoConstraints = false
+		progressBar.topAnchor.constraint(equalTo: self.urlField.bottomAnchor).isActive = true
+		progressBar.heightAnchor.constraint(equalToConstant: 2).isActive = true
+		progressBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+		progressBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.url), options: .new, context: nil)
+		
+		let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+		tap.cancelsTouchesInView = false
+		self.view.addGestureRecognizer(tap)
 	}
 	
 	@objc func prevBtn(_ sender: UIBarButtonItem) {
@@ -73,4 +115,29 @@ class DownloadWebViewController: UIViewController {
 		delegate?.retrievedVideoLink(videoLink: webView.url!.absoluteString)
 	}
 	
+	func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+		let alert = UIAlertController(title: "Error", message:  error.localizedDescription, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "Ok", style: .default,  handler: nil))
+		present(alert, animated: true, completion: nil)
+	}
+		
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		if keyPath == "estimatedProgress" {
+			progressBar.progress = Float(webView.estimatedProgress)
+			progressBar.isHidden = (webView.estimatedProgress == 1.0)
+		} else if keyPath == "URL" {
+			urlField.text = webView.url?.absoluteString
+		}
+	}
+
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		if let urlString = urlField.text {
+			if urlString != webView.url?.absoluteString {
+				webView.load(URLRequest(url: URL(string: urlString)!))
+			}
+		}
+		return true
+	}
+
 }
