a2a231ac2efe161958ce91709d8925618ffe736a
youstanzr <13307813+youstanzr@users.noreply.github.com>
2020-05-13 05:16:16 +0300

    Left align tag view layout

--- YouTag/YYTTagView.swift
+++ YouTag/YYTTagView.swift
@@ -28,7 +28,7 @@ class YYTTagView: UICollectionView, UICollectionViewDataSource, UICollectionView
 	
 	
 	init(frame: CGRect, tagsList: NSMutableArray, isAddEnabled: Bool, isMultiSelection: Bool, isDeleteEnabled: Bool) {
-		super.init(frame: frame, collectionViewLayout: UICollectionViewFlowLayout())
+		super.init(frame: frame, collectionViewLayout: LeftAlignedCollectionViewFlowLayout())
 		self.register(YYTTagCell.self, forCellWithReuseIdentifier: "TagCell")
 		self.delegate = self
 		self.dataSource = self
@@ -205,4 +205,26 @@ class YYTTagView: UICollectionView, UICollectionViewDataSource, UICollectionView
 		self.performBatchUpdates(nil, completion: nil)
 	}
 	
+}
+
+class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+	
+	override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+		let attributes = super.layoutAttributesForElements(in: rect)
+		
+		var leftMargin = sectionInset.left
+		var maxY: CGFloat = -1.0
+		attributes?.forEach { layoutAttribute in
+			if layoutAttribute.frame.origin.y >= maxY {
+				leftMargin = sectionInset.left
+			}
+			
+			layoutAttribute.frame.origin.x = leftMargin
+			
+			leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+			maxY = max(layoutAttribute.frame.maxY , maxY)
+		}
+		
+		return attributes
+	}
 }
