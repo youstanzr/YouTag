cc899554f8cf034ef7fcdc907aaae6a4dec8d220
youstanzr <13307813+youstanzr@users.noreply.github.com>
2020-05-29 15:32:18 +0300

    Update AppDelegate

--- YouTag/AppDelegate.swift
+++ YouTag/AppDelegate.swift
@@ -63,7 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "YoutubeDownloader")
+        let container = NSPersistentContainer(name: "YouTag")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
